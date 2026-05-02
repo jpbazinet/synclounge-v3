eda738260a779c659c69a6fe2f1c0b5a3911ca15
jpbazinet <55415149+jpbazinet@users.noreply.github.com>
2026-05-02 11:33:52 -0400

    Add scroll handling and user scroll detection

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,7 +1,8 @@
 <template>
   <v-list
-    v-chat-scroll
+    ref="listEl"
     density="compact"
+    @scroll.passive="onScroll"
   >
     <v-list-subheader class="md-4">
       Chat
@@ -26,10 +27,41 @@ export default {
     MessageItem: defineAsyncComponent(() => import('@/components/MessageItem.vue')),
   },
 
+  data: () => ({
+    userScrolledUp: false,
+  }),
+
   computed: {
     ...mapGetters('synclounge', [
       'GET_MESSAGES',
     ]),
+  },
+
+  watch: {
+    GET_MESSAGES() {
+      if (!this.userScrolledUp) {
+        this.$nextTick(this.scrollToBottom);
+      }
+    },
+  },
+
+  mounted() {
+    this.$nextTick(this.scrollToBottom);
+  },
+
+  methods: {
+    scrollToBottom() {
+      const el = this.$refs.listEl?.$el ?? this.$el;
+      if (el) {
+        el.scrollTop = el.scrollHeight;
+      }
+    },
+    onScroll(event) {
+      const el = event.target;
+      if (!el) return;
+      const distanceFromBottom = el.scrollHeight - el.scrollTop - el.clientHeight;
+      this.userScrolledUp = distanceFromBottom > 50;
+    },
   },
 };
 </script>
